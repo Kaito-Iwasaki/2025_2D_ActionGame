2989500aa940ae48fd2c8736e00594ba1b2a8016
Kaito-Iwasaki <251307@st.yoshida-g.ac.jp>
2025-10-19 15:35:02 +0900

    レベル出力処理（仮）追加

--- editorblock.cpp
+++ editorblock.cpp
@@ -144,6 +144,21 @@ void UpdateEditorBlock(void)
 
 		SetMap(&MapInfo[0][0]);
 	}
+
+	if (GetKeyboardTrigger(DIK_F3))
+	{
+		MAPINFO MapInfo[NUM_BLOCK_Y][NUM_BLOCK_X];
+
+		for (int y = 0; y < NUM_BLOCK_Y; y++)
+		{
+			for (int x = 0; x < NUM_BLOCK_X; x++)
+			{
+				MapInfo[y][x].type = g_aEditorBlock[y][x].type;
+			}
+		}
+
+		SaveBin("data\\MAP\\level01.bin", &MapInfo[0][0], sizeof(MAPINFO), MAX_BLOCK);
+	}
 }
 
 //=====================================================================
